8e0ec412fd6e629bc80f3b2cc196f76bd4ee50d4
lichangwei <lichangwei@love.com>
2012-12-27 15:51:36 +0800

    control the frequency of rotate and zoom event

--- extend/rotate.js
+++ extend/rotate.js
@@ -8,6 +8,9 @@
 
 var rotations = {};
 
+var rotateMinStep;
+g.opt('rotate_min_step', 1);
+
 g.register('rotate rotateStart', {
   touchstart: g.util.preventDefault,
   touchmove : g.util.preventDefault,
@@ -16,12 +19,17 @@ g.register('rotate rotateStart', {
     var gid = this._gesture_id;
     rotations[gid] = e.rotation;
     g.createEvent('rotateStart', e);
+    rotateMinStep = g.opt('rotate_min_step');
   },
   gesturechange: function(e){
     var gid = this._gesture_id;
     var before  = rotations[gid];
     var current = e.rotation;
-    rotations[gid] = current = adjust(current, before);
+    current = adjust(current, before);
+    if(Math.abs(current - before) < rotateMinStep){
+      return;
+    }
+    rotations[gid] = current;
     g.createEvent('rotate', e, {
       rotation: current
     });
--- extend/zoom.js
+++ extend/zoom.js
@@ -6,16 +6,31 @@
 
 'use strict';
 
+var scales = {};
+var max, min;
+g.opt('zoom_min_step', 1.1);
+
 g.register('zoom zoomStart', {
   touchstart: g.util.preventDefault,
   touchmove : g.util.preventDefault,
   touchend  : g.util.preventDefault,
   gesturestart: function(e){
     g.createEvent('zoomStart', e);
+    scales[this._gesture_id] = 1;
+    var step = g.opt('zoom_min_step');
+    min = 1 / step;
+    max = step;
   },
   gesturechange: function(e){
+    var gid = this._gesture_id;
+    var scale = e.scale;
+    var ratio = scales[gid] / scale;
+    if(ratio > min && ratio < max){
+      return;
+    }
+    scales[gid] = scale;
     g.createEvent('zoom', e, {
-      scale: e.scale
+      scale: scale
     });
   }
 });
