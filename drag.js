5da79b1737e48190c2f42d6a886646267b15e102
lichangwei <lichangwei@love.com>
2012-12-27 11:14:18 +0800

    if the touch has moved more than 30px in 300ms, then drag event won't triggered.

--- drag.js
+++ drag.js
@@ -8,24 +8,64 @@
 
 g.opt('tap_max_duration', 300);
 
-g.prototype.draggable = function(opt){
+/*
+ * @desc define the drag action.
+ * @param {object} opt optinal.
+ *    {function} opt.touchstart.
+ *    {function} opt.touchmove.
+ *      return false then you can position this element yourself.
+ *    {function} opt.touchend.
+ *      return false then you can position this element yourself.
+ *    {string} opt.helper. How to indicate current position.
+ *      'clone': default value. Clone the draggable element.
+ *      'no': Move the draggable element in real time.
+ *    {function} opt.positionShadow.
+ *    {string, object} opt.container. the darggable element cannot move out of this element/area.
+ *      string means it's container's selector of the element.
+ *      object means it's the boundary of the area. typically it contains top, left, width(or right), height(or bottom).
+ */
+g.prototype.drag = function(opt){
   opt = opt || {};
+  opt.helper = opt.helper || 'clone';
   if(typeof opt.container === 'string'){
     opt.container = document.querySelector(opt.container);
   }
   for(var i = 0; i < this.elems.length; i++){
-    var elem = this.elems[i];
-    elem.addEventListener(g.event.touchstart, function(e){
-      var _t = this;
-      this._drag_timeout = setTimeout(function(){
-        ontouchstart.call(_t, e, opt);
-      }, g.opt('tap_max_duration'));
-    }, false);
-    elem.addEventListener(g.event.touchend, function(e){
-      clearTimeout( this._drag_timeout );
-    }, false);
+    bindEvents(this.elems[i], opt);
   }
 };
+
+function bindEvents(elem, opt){
+  var ontouchmove;
+  var timeout;
+  elem.addEventListener(touchstart, function(e){
+    var _t = elem;
+    var startX = getPageX(e);
+    var startY = getPageY(e);
+    
+    ontouchmove = function(e){
+      var endX = getPageX(e);
+      var endY = getPageY(e);
+      var distance = g.util.getDistance([endX, endY], [startX, startY]);
+      if(distance > g.opt('tap_max_distance')){
+        document.removeEventListener(touchmove, ontouchmove);
+        clearTimeout( timeout );
+      }
+    };
+
+    timeout = setTimeout(function(){
+      elem.removeEventListener(touchmove, ontouchmove);
+      ontouchstart.call(_t, e, opt);
+    }, g.opt('tap_max_duration'));
+
+    document.addEventListener(touchmove, ontouchmove, false);
+  }, false);
+  
+  elem.addEventListener(touchend, function(e){
+    document.removeEventListener(touchmove, ontouchmove);
+    clearTimeout( timeout );
+  }, false);
+}
 
 function ontouchstart( e, opt ){
   var me = this;
@@ -51,16 +91,16 @@ function ontouchstart( e, opt ){
   var container = opt.container instanceof HTMLElement ? opt.container.getBoundingClientRect() : opt.container;
 
   if(opt.touchstart){
-    returnValue = opt.touchstart.call(me, e, startTouchX, startTouchY, startElemX, startElemY);
+    opt.touchstart.call(me, e, startTouchX, startTouchY, startElemX, startElemY);
   }
-  if((opt.helper === void 0) || (opt.helper === 'shadow')){
+  if(opt.helper === 'clone'){
     var shadow = me.cloneNode(true);
     shadow.className = me.className;
     (opt.positionShadow || positionShadow).call(shadow, rect.left, rect.top);
     document.body.appendChild(shadow);
   }
   
-  function touchmove(e){
+  function ontouchmove(e){
     e.preventDefault();
     // get left and top of touch/mouse
     touchX = getPageX(e);
@@ -82,33 +122,36 @@ function ontouchstart( e, opt ){
     if(opt.touchmove){
       returnValue = opt.touchmove.call(me, e, elemX, elemY, absoluteElemX - rect.left, absoluteElemY - rect.top, e.timeStamp-startT);
     }
-    if((opt.helper === void 0) || (opt.helper === 'shadow')){
+    if(opt.helper === 'clone'){
       (opt.positionShadow || positionShadow).call(shadow, absoluteElemX, absoluteElemY);
     }else if(returnValue !== false){
       me.style.left = elemX + 'px';
       me.style.top  = elemY + 'px';
     }
   }
-  function touchend(e){
+  function ontouchend(e){
     e.preventDefault();
-    document.removeEventListener(g.event.touchmove, touchmove);
-    document.removeEventListener(g.event.touchend,  touchend);
+    document.removeEventListener(touchmove, ontouchmove);
+    document.removeEventListener(touchend,  ontouchend);
     if(opt.touchend){
       returnValue = opt.touchend.call(me, e, elemX, elemY, absoluteElemX - rect.left, absoluteElemY - rect.top, e.timeStamp-startT);
     }
-    if((opt.helper === void 0) || (opt.helper === 'clone')){
+    if(opt.helper === 'clone'){
       document.body.removeChild(shadow);
     }
     if(returnValue === false) return;
     me.style.left = elemX + 'px';
     me.style.top  = elemY + 'px';
   }
-  document.addEventListener(g.event.touchmove, touchmove, false);
-  document.addEventListener(g.event.touchend,  touchend,  false);
+  document.addEventListener(touchmove, ontouchmove, false);
+  document.addEventListener(touchend,  ontouchend,  false);
 }
 
 var getPageX = g.util.getPageX;
 var getPageY = g.util.getPageY;
+var touchstart = g.event.touchstart;
+var touchmove = g.event.touchmove;
+var touchend = g.event.touchend;
 
 function positionShadow(left, top){
   this.style.cssText = [
